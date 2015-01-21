0e74bc25824b64c20c12b2ef8e8cd7ce15e4764b
MaZderMind <github@mazdermind.de>
2015-01-21 06:45:34 +0100

    fix server a little

--- dbus-signal-ownsrv.c
+++ dbus-signal-ownsrv.c
@@ -117,7 +117,8 @@ static gboolean
 gst_switch_controller_on_new_connection (GDBusServer * server,
     GDBusConnection * connection, gpointer user_data)
 {
-  printf("new-connection...");
+  printf("new-connection...\n");
+  g_object_ref (connection);
   return TRUE;
 }
 
@@ -137,12 +138,12 @@ int main(int argc, char * argv[])
     GDBusAuthObserver *auth_observer;
 
     GDBusServerFlags flags = G_DBUS_SERVER_FLAGS_NONE;
-    //flags |= G_DBUS_SERVER_FLAGS_RUN_IN_THREAD;
+    flags |= G_DBUS_SERVER_FLAGS_RUN_IN_THREAD;
     flags |= G_DBUS_SERVER_FLAGS_AUTHENTICATION_ALLOW_ANONYMOUS;
 
     auth_observer = g_dbus_auth_observer_new ();
     printf("g_dbus_server_new_sync\n");
-    bus_server = g_dbus_server_new_sync ("tcp:port=5000",
+    bus_server = g_dbus_server_new_sync ("tcp:host=0.0.0.0,port=5000",
                                          flags,
                                          guid,
                                          auth_observer,
@@ -158,10 +159,13 @@ int main(int argc, char * argv[])
     g_signal_connect (bus_server, "new-connection",
       G_CALLBACK (gst_switch_controller_on_new_connection), NULL);
 
+    g_dbus_server_start (bus_server);
+    g_object_unref (auth_observer);
+
     g_free (guid);
 
 
-#if 0
+
     printf("g_dbus_connection_new_for_address_sync\n");
     GDBusConnection * con = g_dbus_connection_new_for_address_sync("tcp:host=127.0.0.1,port=5000",
                                                                     G_DBUS_CONNECTION_FLAGS_NONE,
@@ -170,9 +174,6 @@ int main(int argc, char * argv[])
                                                                     &error);
 
     g_assert_no_error (error);
-
-    g_dbus_server_start (bus_server);
-    g_object_unref (auth_observer);
 
 
 
@@ -208,17 +209,17 @@ int main(int argc, char * argv[])
                                              /*user_data_free_func*/ NULL);
 
     printf("g_bus_own_name done, entering mainloop\n");
-#endif
+
     loop = g_main_loop_new (NULL, FALSE);
     g_main_loop_run (loop);
 
-#if 0
+
     g_dbus_connection_unregister_object(con, registration_id);
 
     g_bus_unown_name (owner_id);
 
     g_dbus_node_info_unref (introspection_data);
-#endif
+
     g_dbus_server_stop (bus_server);
     g_object_unref (bus_server);
 
